0dbe34caf6a25e30a094ae0cacccc5c6396c9ddb
Gospel-0010 <deepak.k.0010@gmail.com>
2015-09-10 03:08:46 -0600

    Create Udp_WiFi.c

---
+++ Udp_WiFi.c
@@ -0,0 +1,32 @@
+/* 
+  Gets the time data from NTP server by UDP.
+
+  You can change the NTP server address by modify g_address.
+*/
+#include <string_t.h>
+#include "vmtype.h" 
+#include "vmboard.h"
+#include "vmsystem.h"
+#include "vmlog.h" 
+#include "vmcmd.h" 
+#include "vmdcl.h"
+#include "vmdcl_gpio.h"
+#include "vmthread.h"
+
+#include "ResID.h"
+#include "UDP.h"
+#include "vmsock.h"
+#include "vmbearer.h"
+#include "vmudp.h"
+#include "vmtimer.h"
+
+#include "vmwlan.h"
+
+
+
+void vm_main(void) 
+{
+	wlan_connect();
+	vm_timer_create_non_precise(60000, send_udp, NULL);
+}
+
